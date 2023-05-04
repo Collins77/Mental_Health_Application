115b53a231ffe7674d4fc42c2793a70a541a259f
Collins77 <collinsmuemah@gmail.com>
2023-05-04 15:23:04 +0300

    updates

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const Color mentalDarkColor = Color(0xFF000000);
+}
--- lib/launchscreen/presentation/welcome_screen.dart
+++ lib/launchscreen/presentation/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_app/core/theme/app_colors.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -6,6 +7,7 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.mentalDarkColor,
       body: Center(
         child: Text("Welcome to Flutter"),
       ),

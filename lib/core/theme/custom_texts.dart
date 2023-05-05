5485c9fc9fe1e65fff16c9eee697c341ad2e3de7
Collins77 <collinsmuemah@gmail.com>
2023-05-05 15:37:28 +0300

    updates

--- lib/core/theme/custom_texts.dart
+++ lib/core/theme/custom_texts.dart
@@ -1,6 +1,6 @@
-
 class CustomText {
   CustomText._();
   static const String brand_name_1 = 'Mental';
   static const String brand_name_2 = 'health';
+  static const String skipText = 'Skip';
 }
--- lib/onboard/presentation/onboard_screen.dart
+++ lib/onboard/presentation/onboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:mental_health_app/core/theme/app_colors.dart';
+import 'package:mental_health_app/core/theme/custom_texts.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({Key? key}) : super(key:key);
@@ -14,7 +15,34 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: AppColors.mentalBrandLightColor,
-      body: SafeArea(child: Text("Welcome to the onboarding screen")),
+      body: SafeArea(
+        child: PageView(
+        children: [
+          Container(
+            padding: EdgeInsets.all(18.0),
+          child: 
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(CustomText.skipText,
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20.0,
+                    color: AppColors.mentalDarkColor,
+                  ),)
+                ],
+              ),
+            ],
+          ),),
+          Container(color: Colors.red,),
+          Container(color: Colors.blue,),
+          Container(color: Colors.green,),
+        ],
+      )
+      ),
     );
   }
 

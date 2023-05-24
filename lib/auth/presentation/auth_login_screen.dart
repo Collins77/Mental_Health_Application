6d2654dc260a7feda03c83133d722b5cfa4783d6
Collins77 <collinsmuemah@gmail.com>
2023-05-24 12:33:36 +0300

    updates

--- lib/auth/presentation/auth_login_screen.dart
+++ lib/auth/presentation/auth_login_screen.dart
@@ -60,11 +60,96 @@ class _AuthLoginScreenState extends State<AuthLoginScreen> {
                             return null;
                           },
                         ),
+                        customSizedBox(context: context, size: 0.02),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                'Forgot password?',
+                                textAlign: TextAlign.right,
+                                style: TextStyle(
+                                  fontSize: 13.0,
+                                  height: 1.2,
+                                  fontFamily: 'Inter',
+                                  fontWeight: FontWeight.w200,
+                                  color: AppColors.mentalDarkColor,
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                         customSizedBox(context: context, size: 0.1),
                         CustomBtn(
-                          onPressed: () => {}, 
+                          onPressed: () => {},
                           buttonText: 'Sign In',
-                          )
+                        ),
+                         customSizedBox(context: context, size: 0.13),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'or sign in with',
+                              style: TextStyle(
+                                color: AppColors.mentalDarkColor,
+                              ),
+                              ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                CustomCirclerBtn(
+                                  imgName: 'icongoogle', 
+                                  onPressed: () => {}, 
+                                  redus: 20.0,
+                                  bagroundRadius: 22.0,
+                                ),
+                                CustomCirclerBtn(
+                                  imgName: 'iconfacebook', 
+                                  onPressed: () => {},
+                                  redus: 20.0,
+                                  bagroundRadius: 22.0,
+                                ),
+                                CustomCirclerBtn(
+                                  imgName: 'iconapple', 
+                                  onPressed: () => {},
+                                  redus: 20.0,
+                                  bagroundRadius: 22.0,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        customSizedBox(context: context, size: 0.1),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Don\'t have an account?',
+                              style: TextStyle(
+                                fontSize: 13.0,
+                                height: 1.2,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w200,
+                                color: AppColors.mentalDarkColor,
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                'Sign up',
+                                style: TextStyle(
+                                fontSize: 13.0,
+                                height: 1.2,
+                                fontFamily: 'Inter',
+                                fontWeight: FontWeight.w600,
+                                color: AppColors.mentalBrandColor,
+                              ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   )
@@ -77,3 +162,5 @@ class _AuthLoginScreenState extends State<AuthLoginScreen> {
     );
   }
 }
+
+
--- lib/uis/custom_buttons.dart
+++ lib/uis/custom_buttons.dart
@@ -5,36 +5,70 @@ import '../core/theme/app_colors.dart';
 class CustomBtn extends StatelessWidget {
   final VoidCallback onPressed;
   final String buttonText;
-  const CustomBtn({super.key, required this.onPressed, required this.buttonText});
+  const CustomBtn(
+      {super.key, required this.onPressed, required this.buttonText});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-            onTap: onPressed,
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.only(
-                top: 18.0,
-                left: 18.0,
-                right: 18.0,
-                bottom: 18.0,
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.0),
-                color: AppColors.mentalBrandColor,
-              ),
-              child: Center(
-                child: Text(
-                  buttonText,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 20.0,
-                    color: AppColors.mentalBrandLightColor,
-                  ),
-                ),
-              ),
+      onTap: onPressed,
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(
+          top: 18.0,
+          left: 18.0,
+          right: 18.0,
+          bottom: 18.0,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.0),
+          color: AppColors.mentalBrandColor,
+        ),
+        child: Center(
+          child: Text(
+            buttonText,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 20.0,
+              color: AppColors.mentalBrandLightColor,
             ),
-          );
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomCirclerBtn extends StatelessWidget {
+  final String imgName;
+  final VoidCallback onPressed;
+  final double redus;
+  final double bagroundRadius;
+  const CustomCirclerBtn(
+      {super.key,
+      required this.imgName,
+      required this.onPressed,
+      required this.redus, required this.bagroundRadius});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10.0, top: 15.0),
+      child: GestureDetector(
+        onTap: onPressed,
+        child: CircleAvatar(
+          backgroundColor: AppColors.mentalBrandColor,
+          radius: bagroundRadius,
+          child: CircleAvatar(
+            radius: redus,
+            child: Image.asset(
+              'assets/images/$imgName.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

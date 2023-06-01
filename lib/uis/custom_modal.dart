022563e46b73eae4852e92a372bbb425a2ad084b
Collins77 <collinsmuemah@gmail.com>
2023-06-01 08:18:47 +0300

    updates

--- lib/uis/custom_modal.dart
+++ lib/uis/custom_modal.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:mental_health_app/core/theme/custom_texts.dart';
+import 'package:mental_health_app/uis/custom_text.dart';
 
 void customBottomSheet(
-    {required BuildContext context,
+    {
+      required BuildContext context,
     required String title,
     required Widget content,
     required VoidCallback onPressed}) {
   showModalBottomSheet(
       context: context,
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.35,
+        minWidth: MediaQuery.of(context).size.width,
+      ),
       builder: (BuildContext context) {
         return Container(
           child: Column(
             children: [
-              Text('Here is a modal')
+              mainHeading(
+                text: CustomText.mentalPasswordResetText, 
+                context: context,
+                )
             ],
           ),
         );
